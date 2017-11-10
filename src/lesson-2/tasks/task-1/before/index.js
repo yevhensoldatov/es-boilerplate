e0d14ca6687abb9d7e72d91692b233e8832c82a0
Andrey Prisniak <a.prisniak@lectrum.io>
2017-11-10 17:48:12 +0200

    add description for task-2 lesson-2

--- src/lesson-2/tasks/task-1/before/index.js
+++ src/lesson-2/tasks/task-1/before/index.js
@@ -7,6 +7,7 @@ const pattern = /is a miracle/y;
 
 console.log(findString(text, pattern));
 
+// Important: Task should be done with the help of lastIndex property. Split method is restricted.
 // The function should count how many times `is a miracle` is met in a string.
 
 
