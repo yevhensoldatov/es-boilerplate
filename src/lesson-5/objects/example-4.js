35d1d901597c012a7becf7e839028dd7a23aed79
Andrey Prisniak <a.prisniak@lectrum.io>
2017-11-17 03:00:33 +0200

    add lesson-5

---
+++ src/lesson-5/objects/example-4.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// ES5
+function createNewPerson(name, age) {
+    return {
+        name,
+        age
+    }
+}
+console.log(createNewPerson('Jon', 25));
+
+// ES6
+const createHuman = (name, age) => ({ name, age });
+console.log(createHuman('Joe', 24));
---
+++ src/lesson-5/tasks/task-1/before/index.js
@@ -0,0 +1,33 @@
+import {
+    Entity,
+    talk,
+    age
+} from '../after';
+
+const bender = new Entity({
+    id:        2716057,
+    firstName: 'Bender',
+    surName:   'Bending',
+    lastName:  'Rodriguez',
+    sex:       'robot'
+});
+
+Object.setPrototypeOf(bender, talk);
+
+bender.say();
+bender.sayAge(); // hm undefined? Lets fix it...
+
+Object.setPrototypeOf(bender, age);
+bender.fixAge(4) // now I'm 4 years old, let's check it...
+
+Object.setPrototypeOf(bender, talk);
+bender.sayAge(); // no more undefined :)
+
+// But now I can not change my age, and I can not use setPrototypeOf function anymore...
+// I need to have methods from two prototypes in one entity, hm...
+// But I can not do this in JavaScript, or can?
+
+// <- remove this and write code here
+Object.assign(talk, age);
+bender.fixAge(5);
+bender.sayAge(); // Happy birthday, now I'm five years old.
