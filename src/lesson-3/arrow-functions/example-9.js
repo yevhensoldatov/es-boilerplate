159f9de52b25dc6ec6b87785f0769ee69b61f520
amulyk <a.mulyk@lectrum.io>
2017-11-17 13:48:15 +0200

    fixed issues on lesson 3

--- src/lesson-3/arrow-functions/example-9.js
+++ src/lesson-3/arrow-functions/example-9.js
@@ -4,11 +4,16 @@ const data = {
     value: 0,
     getValue: function () {
         return () => this.value;
+    },
+    getValue2: function () {
+        return function () {
+            return this.value;
+        }
     }
 }
 
 const newData = {
-    data: 1
+    value: 1
 }
 
 const f1 = data.getValue().bind(newData);
