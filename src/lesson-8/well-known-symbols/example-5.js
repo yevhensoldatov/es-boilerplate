881b4fd3992cdbe72137b94c92fe9b38173b5adb
amulyk <a.mulyk@lectrum.io>
2017-11-27 19:55:55 +0200

    added task

--- src/lesson-8/well-known-symbols/example-5.js
+++ src/lesson-8/well-known-symbols/example-5.js
@@ -6,6 +6,6 @@ function Temperature(degrees) {
 
 const freezing = new Temperature(32);
 
-console.log(freezing + '!'); // [object Object]!
-console.log(freezing / 2); // NaN
-console.log(String(freezing)); // [object Object]
+console.log(freezing + '!'); // [object Object]! → // 32 degrees!
+console.log(freezing / 2); // NaN → // 16
+console.log(String(freezing)); // [object Object] → // 32°
